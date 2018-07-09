a94254a770835edd2ac0ddfc418b6ba3390cf9ca
Weinilin <weinilinhappy@gmail.com>
2018-07-09 17:15:07 +0800

    add in top 20 dummy datas and update of UI for recycler view

--- app/src/main/java/com/example/aw/capplication/DashboardFragment.java
+++ app/src/main/java/com/example/aw/capplication/DashboardFragment.java
@@ -3,9 +3,19 @@ package com.example.aw.capplication;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.aw.capplication.Adapter.TopicAdapter;
+import com.example.aw.capplication.Model.Topic;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 /**
  * Created by Aw on 9/7/2018.
@@ -14,6 +24,13 @@ import android.view.ViewGroup;
 public class DashboardFragment extends Fragment {
     private String username = "";
     private static final int DIALOG_FRAGMENT = 1;
+
+    private Toolbar toolbar;
+    private RecyclerView mTopicRecyclerView;
+    private ArrayList<Topic> mTopics = new ArrayList<>();
+    private ArrayList<Topic> top20Topics = new ArrayList<>();
+    private static final DateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy");
+    private TopicAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
@@ -40,5 +57,43 @@ public class DashboardFragment extends Fragment {
                                    }
                                }
         );
+
+
+        //add in 20 topics
+        for (int i = 0; i < 20; i++) {
+            Topic topic = new Topic();
+            topic.setNumOfUpVote("0");
+            topic.setNumOfDownVote("0");
+            topic.setUsername("Brenda" + Integer.toString(i));
+            topic.setContent("Topic of the day \n" + Integer.toString(i));
+
+            mTopics.add(topic);
+
+        }
+
+        //always only display top 20 even after user add addition
+        setTop20();
+
+        mTopicRecyclerView = (RecyclerView) view.findViewById(R.id.topics_recycler_view);
+
+        updateUI();
+    }
+
+    private void setTop20() {
+        top20Topics.clear();
+        for (int i = 0; i < mTopics.size(); i++) {
+            if (i == 20) {
+                break;
+            }
+            top20Topics.add(mTopics.get(i));
+
+        }
+    }
+
+    private void updateUI() {
+        mAdapter = new TopicAdapter(top20Topics);
+        mTopicRecyclerView.setAdapter(mAdapter);
+        // Set layout manager to position the items
+        mTopicRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 }
