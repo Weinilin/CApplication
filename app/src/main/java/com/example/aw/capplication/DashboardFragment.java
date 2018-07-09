2f88e33b84df0a088c61d2f22823d5e31c005590
Weinilin <weinilinhappy@gmail.com>
2018-07-09 17:49:24 +0800

    Implement add topic after sign in

--- app/src/main/java/com/example/aw/capplication/DashboardFragment.java
+++ app/src/main/java/com/example/aw/capplication/DashboardFragment.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.aw.capplication.Adapter.TopicAdapter;
+import com.example.aw.capplication.Dialog.SignInDialogFragment;
 import com.example.aw.capplication.Model.Topic;
 
 import java.text.DateFormat;
--- app/src/main/java/com/example/aw/capplication/Dialog/AddTopicFragment.java
+++ app/src/main/java/com/example/aw/capplication/Dialog/AddTopicFragment.java
@@ -1,7 +1,9 @@
-package com.example.aw.capplication;
+package com.example.aw.capplication.Dialog;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
@@ -11,31 +13,46 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.example.aw.capplication.DashboardFragment;
+import com.example.aw.capplication.R;
+
 /**
  * Created by Aw on 9/7/2018.
  */
 
-public class SignInDialogFragment  extends DialogFragment {
+public class AddTopicFragment extends DialogFragment {
+    String finalUsername;
+    String finalPassword;
 
     @Override
     @NonNull
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         // Get the layout inflater
-        final LayoutInflater inflater = getActivity().getLayoutInflater();
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+
+        if (getArguments() != null) {
+
+            finalUsername = getArguments().getString(getResources().getString(R.string.USERNAME_TEXT),
+                    "");
+            finalPassword = getArguments().getString(getResources().getString(R.string.PASSWORD_TEXT), "");
+        }
         // Inflate and set the layout for the dialog
         // Pass null as the parent view because its going in the dialog layout
 
-        builder.setView(inflater.inflate(R.layout.dialog_signin, null))
+        builder.setView(inflater.inflate(R.layout.dialog_add_topic, null))
                 // Add action buttons
-                .setPositiveButton(R.string.LOGIN_TEXT, new DialogInterface.OnClickListener() {
+                .setPositiveButton(R.string.ADD_TEXT, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
+
+
                     }
                 })
                 .setNegativeButton(R.string.CANCEL_TEXT, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        SignInDialogFragment.this.getDialog().cancel();
+                        AddTopicFragment.this.getDialog().cancel();
                     }
                 });
         return builder.create();
@@ -50,25 +67,20 @@ public class SignInDialogFragment  extends DialogFragment {
             positiveButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    // sign in the user ...
-                    EditText username_edittext = getDialog().findViewById(R.id.username_textview);
+                    EditText content_edittext = getDialog().findViewById(R.id.content_edittext);
+                    String content = content_edittext.getText().toString();
 
-                    String username = username_edittext.getText().toString();
-                    EditText password_edittext = getDialog().findViewById(R.id.password_textview);
-                    String password = password_edittext.getText().toString();
-                    if (username != null && !username.equals("")) {
-                        //prompt adding of topic
+                    Bundle bundle = new Bundle();
+                    bundle.putString(getResources().getString(R.string.USERNAME_TEXT), finalUsername);
+                    bundle.putString(getResources().getString(R.string.PASSWORD_TEXT), finalPassword);
+                    bundle.putString(getResources().getString(R.string.TOPIC_CONTENT_TEXT), content);
 
-
-                    } else {
-                        username_edittext.setHint("Please enter username");
-                        username_edittext.requestFocus();
-                    }
 
                 }
             });
         }
     }
 }
+
 
 
